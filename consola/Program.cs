63bd6e3eba56984d62325d510afb05b7ed299b04
camargo.enrique@uabc.edu.mx <camargo.enrique@uabc.edu.mx>
2021-08-26 19:20:08 -0700

    Se agrego consola y web

--- consola/Program.cs
+++ consola/Program.cs
@@ -10,6 +10,7 @@ namespace consola
     {
         static void Main(string[] args)
         {
-        }
+            Console.WriteLine("hello word");
+            Console.ReadKey();        }
     }
 }
